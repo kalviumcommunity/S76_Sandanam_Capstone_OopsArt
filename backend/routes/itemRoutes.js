baa7d8b3a344d550552de2323fb376ad4bb68468
sandanam <santhanamk9604@gmail.com>
2025-05-06 14:47:44 +0530

    added db

---
+++ backend/routes/itemRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/item');
+
+// Create an Item
+router.post('/items', async (req, res) => {
+  try {
+    const newItem = new Item(req.body);
+    await newItem.save();
+    res.status(201).json(newItem);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Get all Items
+router.get('/items', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const itemRoutes = require('./routes/itemRoutes');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api', itemRoutes);
+
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('✅ MongoDB Connected');
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('❌ Connection failed', err);
+  });
+
